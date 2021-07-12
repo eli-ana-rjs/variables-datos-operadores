399d2f522c1309b061e4cf6cf42b581486f4c7ab
elianarojas <elianabellarojas@gmail.com>
2021-07-12 19:30:47 -0300

    Revert "Subir ejercicio heladería"
    This reverts commit 2b90e2ce494aa5659b5870a2b531950759bb0dbd.

--- index.js
+++ index.js
@@ -1,32 +1,13 @@
-const btnSaludo = document.getElementById('btnSaludo');
-const btnHeladeria = document.getElementById('btnHeladeria');
-
-
-// Ejercicio saludo
+const saludo = document.getElementById('saludo');
 
 const saludar = (nombre, apellido) => {
     const saludo = `"Hola ${nombre} ${apellido}, bienvenida a Ada"`;
     return saludo;
 }
-
-btnSaludo.addEventListener ('click' , () => {
+saludo.addEventListener ('click' , () => {
     const nombre = prompt("Ingrese su nombre");
     const apellido = prompt("Ingrese su apellido");
 
     alert(saludar(nombre, apellido));
     
-});
-
-// Ejercicio heladeria
-
-btnHeladeria.addEventListener( 'click' , () => {
-
-    const gusto1 = prompt('Elija el primer gusto de helado: ');
-    const gusto2 = prompt('Elija el segundo gusto de helado: ');
-    const gusto3= prompt('Elija el tercer gusto de helado: ');
-
-    const mensaje = `Aquí tiene su helado de ${gusto1}, ${gusto2} y ${gusto3}`;
-
-    return alert(mensaje);
-
 });

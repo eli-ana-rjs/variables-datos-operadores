0d4afe784b67123eb37913fb8cca0b2bac0eb4f9
elianarojas <elianabellarojas@gmail.com>
2021-07-12 23:39:45 -0300

    Subir ejercicio Datos Personales

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const btnSaludo = document.getElementById('btnSaludo');
-const btnHeladeria =  document.getElementById('btnHeladeria');
+const btnSaludo = document.getElementById('btn-saludo');
+const btnHeladeria =  document.getElementById('btn-heladeria');
+const btnDatosPersonales = document.getElementById('btn-datos-personales')
 
 const saludar = (nombre, apellido) => {
     const saludo = `"Hola ${nombre} ${apellido}, bienvenida a Ada"`;
@@ -23,3 +24,21 @@ btnHeladeria.addEventListener('click', () => {
 
     return alert(mensaje);
 });
+
+btnDatosPersonales.addEventListener('click', () => {
+
+    const nombre = prompt('Ingrese su nombre: ');
+    const apellido = prompt('Ingrese su apellido: ');
+    const edad = prompt('Ingrese su edad: ');
+    const nacionalidad = prompt('Ingrese su nacionalidad: ');
+    const dni = prompt('Ingrese su número de documento: ');
+
+    const mensaje = `Nuevo usuario agregado al sistema
+    Nombre y apellido = ${nombre} ${apellido}
+    Edad = ${edad}
+    Nacionalidad = ${nacionalidad}`;
+
+    return alert(mensaje);
+
+});
+
